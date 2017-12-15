01c5cd8c0aaa00a3ab55977989be73313b75acc2
MilandaYuan <nwhals@163.com>
2017-12-15 22:19:07 +0800

    first commit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -6,17 +6,17 @@
 /* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
  * 我们得保证在 DOM 准备好之前他们不会被运行。
  */
-$(function() {
+$(function () {
     /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', function () {
         /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
          * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
          * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
          * 页面看看会发生什么。
         */
-        it('are defined', function() {
+        it('are not empty', function () {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -25,37 +25,43 @@ $(function() {
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
-        it('each url is defined and has value', function(){
-            for(var i=0;i<allFeeds.length;i++){
+
+        function sameDetection(key) {
+            for (i = 0; i < allFeeds.length; i++) {
                 var currentFeed = allFeeds[i];
-                expect(currentFeed.url).toBeDefined();
-                expect(currentFeed.url).not.toBe('')
+                expect(currentFeed[key]).toBeDefined();
+                expect(currentFeed[key]).not.toBe('');
+                if (key === "url") {
+                    var regularExpressionUrl = /^((ht|f)tps?):\/\/([\w\-]+(\.[\w\-]+)*\/)*[\w\-]+(\.[\w\-]+)*\/?(\?([\w\-\.,@?^=%&:\/~\+#]*)+)?/;
+                    expect(currentFeed[key]).toMatch(regularExpressionUrl)
+                }
             }
+        }
+
+        it('URLs are not empty', function () {
+            sameDetection("url")
         });
+
 
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
-        it('each name is defined and has value', function(){
-            for(var i=0;i<allFeeds.length;i++){
-                var currentFeed = allFeeds[i];
-                expect(currentFeed.name).toBeDefined();
-                expect(currentFeed.name).not.toBe('')
-            }
-        });
+        it('Names are not empty', function () {
+            sameDetection("name")
+        })
 
     });
 
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
-    describe('The menu',function(){
+    describe('The menu', function () {
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
 
-        it('should be hidden by default',function(){
-               expect($('body').attr('class')).toBe('menu-hidden')
+        it('should be hidden by default', function () {
+            expect($('body').hasClass('menu-hidden')).toBe(true)
         });
 
         /* TODO:
@@ -64,20 +70,21 @@ $(function() {
          * 再次点击的时候是否隐藏。
          */
 
-        it('should be toggled visible or hidden status when menu icon was clicked ',function(){
-            var currentClass = $('body').attr('class');
-            if(currentClass){
-                expect(currentClass).toBe('menu-hidden');
-            }else{
-                expect(currentClass).toBe('');
-            }
-        })
 
+        it('should be toggled visible or hidden status when menu icon was clicked ', function () {
+            var icon = $('.icon-list');
+            var body = $('body');
+            icon.trigger('click');
+            expect(body.hasClass('menu-hidden')).toBe(false);
+            icon.trigger('click');
+            expect(body.hasClass('menu-hidden')).toBe(true)
+
+        })
     });
 
 
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
-    describe('Initial Entries',function(){
+    describe('Initial Entries', function () {
         /* TODO:
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
          * 里面至少有一个 .entry 的元素。
@@ -88,14 +95,11 @@ $(function() {
 
 
         beforeEach(function (done) {
-            loadFeed(0,function (){
-                done()
-            })
+            loadFeed(0, done)
         });
 
-        it('should not be empty ',function(done){
+        it('should not be empty ', function () {
             expect($('.feed').html()).not.toBe('');
-            done()
         })
 
 
@@ -104,30 +108,31 @@ $(function() {
 
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
 
-    describe('New Feed Selection',function(){
+    describe('New Feed Selection', function () {
         /* TODO:
          * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
          * 记住，loadFeed() 函数是异步的。
          */
 
         //获取一个随机数，在beforeEach里增加它的值，将这两个数值分别作为loadFeed的两个ID参数传入，以确定不同的ID是否获得不同的目录内容
-        var initID = Math.floor(Math.random()*(allFeeds.length-1));
-        loadFeed(initID);
-        var initEntries = $('.feed').html();
 
-        beforeEach(function(done){
-            loadFeed(initID+1,function () {
-                done()
-            });
+        var formerEntries;
+        var newEntries;
+        var formerID = Math.floor(Math.random() * (allFeeds.length - 1));
+
+        beforeEach(function (done) {
+            loadFeed(formerID, function () {
+                formerEntries = $('.feed').html();
+                loadFeed(formerID + 1, function () {
+                    newEntries = $('.feed').html();
+                    done()
+                })
+            })
         });
 
-        it('should be different ',function(done){
-            var currentEntries = $('.feed').html();
-            expect(currentEntries).not.toBe(initEntries);
-            done();
+        it('should be different', function () {
+            expect(newEntries).not.toBe(formerEntries)
         })
-
-
     })
 
 }());
